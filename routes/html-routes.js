6fa9e3a5a6ac2295fa14957581927f9cd1d9f62a
Anastasia <nastia.iv.91@gmail.com>
2021-01-18 18:24:46 -0800

    handlebars test2

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,5 +1,6 @@
 // Requiring path to so we can use relative routes to our HTML files
 const path = require("path");
+var user = require('../models/user.js');
 
 // Requiring our custom middleware for checking if a user is logged in
 const isAuthenticated = require("../config/middleware/isAuthenticated");
@@ -24,6 +25,10 @@ module.exports = function(app) {
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
   app.get("/members", isAuthenticated, (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/members.html"));
+    // user.findOne(function(data) {
+      var hbsObject = {name: 'vanilla', price: 10, awesomeness: 3};
+      res.render('members', hbsObject);
+    
+    res.sendFile(path.join(__dirname, "../public/members.handlebars"));
   });
 };
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 // Requiring necessary npm packages
 const express = require("express");
 const session = require("express-session");
+var exphbs = require("express-handlebars");
+
 // Requiring passport as we've configured it
 const passport = require("./config/passport");
 
@@ -13,6 +15,8 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 // We need to use sessions to keep track of our user's login status
 app.use(
   session({ secret: "keyboard cat", resave: true, saveUninitialized: true })

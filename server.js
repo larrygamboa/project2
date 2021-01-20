53a4a38a26cadb5d4ff8ed0919880373b475453a
Larry Gamboa <techmedia.lg@gmail.com>
2021-01-19 21:30:49 -0800

    add nodemailer js code to server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Requiring necessary npm packages
 const express = require("express");
 const session = require("express-session");
+const nodemailer = require("nodemailer");
 var exphbs = require("express-handlebars");
 
 // Requiring passport as we've configured it
@@ -27,6 +28,44 @@ app.use(passport.session());
 // Requiring our routes
 require("./routes/html-routes.js")(app);
 require("./routes/api-routes.js")(app);
+
+// ========== NODEMAILER ========== //
+// async..await is not allowed in global scope, must use a wrapper
+async function main() {
+  // Generate test SMTP service account from ethereal.email
+  // Only needed if you don't have a real mail account for testing
+  let testAccount = await nodemailer.createTestAccount();
+
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    auth: {
+      user: "zane.ratke30@ethereal.email",
+      pass: "H7Qh5b2Ab77JwVWfcx",
+    },
+  });
+
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: '"Zane Ratke" <zane.ratke30@ethereal.email>', // sender address
+    to: "feelthehousegroove@yahoo.com", // list of receivers
+    subject: "Hello, this is a test email", // Subject line
+    text: "Hello world. This is a test!!", // plain text body
+    html: "<b>Hello world. This is a test!!</b>", // html body
+  });
+
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+  // Preview only available when sending through an Ethereal account
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+}
+
+main().catch(console.error);
+
+// ========== END NODEMAILER ========== //
 
 // Syncing our database and logging a message to the user upon success
 db.sequelize.sync().then(() => {

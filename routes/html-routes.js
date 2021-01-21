ca78fdbe64ee5e79d8f82287d7ef23a45c247444
Anastasia <nastia.iv.91@gmail.com>
2021-01-20 20:34:04 -0800

    added complete button functionality, route UPDATE works

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -27,9 +27,7 @@ module.exports = function(app) {
   app.get("/members", isAuthenticated, (req, res) => {
     db.Topics.findAll({}).then(function(dbTopics) {
       db.Activities.findAll({}).then(function(dbActivities) { 
-
            res.render("members", {Topics: dbTopics, Activities: dbActivities})
-    //  console.log("**************",dbActivities)
       })
   });
   });
@@ -37,28 +35,41 @@ module.exports = function(app) {
 
   app.post("/api/insertTopic", function(req, res) {
     db.Topics.create({topic_name: req.body.name, t_condition: false}).then(function() {
-      console.log("*************", req.body)
         res.redirect("/members");
       });
   });
   
   app.post("/api/insertActivity", function(req, res) {
     db.Activities.create({activity_name: req.body.name, a_condition: false}).then(function() {
-      console.log("*************", req.body)
         res.redirect("/members");
       });
   });
 
  
-  app.put("/api/updateTopic", function(req, res) {
+  app.put("/api/updateTopic/:id", function(req, res) {
     db.Topics.update(
       {t_condition: true},
       {
         where: {
-          id: req.body.id
+          id: req.params.id
         }
       }).then(function() {
-        res.redirect("/members");
+       
+        res.sendStatus(200);
+    });
+  });
+
+
+  app.put("/api/updateActivity/:id", function(req, res) {
+    db.Activities.update(
+      {a_condition: true},
+      {
+        where: {
+          id: req.params.id
+        }
+      }).then(function() {
+       
+        res.sendStatus(200);
     });
   });
 };

42968e0d1e2c995c28dac0d30bdf15917aa1d89e
Larry Gamboa <techmedia.lg@gmail.com>
2021-01-25 20:32:32 -0800

    updates to server.js

--- server.js
+++ server.js
@@ -8,29 +8,29 @@ var exphbs = require("express-handlebars");
 // Requiring passport as we've configured it
 const passport = require("./config/passport");
 
-// Required dependencies
-const mysql = require("mysql");
-let connection;
-// Connect to database
-if (process.env.JAWSDB_URL) {
-  var connection = mysql.createConnection(process.env.JAWSDB_URL)
-} else {
-  var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'l@rG020979!',
-    database: 'passport_demo'
-  });
-}
+// // Required dependencies
+// const mysql = require("mysql");
+// let connection;
+// // Connect to database
+// if (process.env.JAWSDB_URL) {
+//   var connection = mysql.createConnection(process.env.JAWSDB_URL)
+// } else {
+//   var connection = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: 'l@rG020979!',
+//     database: 'passport_demo'
+//   });
+// }
 
-// Make sure connection works
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+// // Make sure connection works
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });
 
 // Setting up port and requiring models for syncing
 const PORT = process.env.PORT || 8080;

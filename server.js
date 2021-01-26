157674fadcce32c6ffc755bce00e21d8e14bd9db
Anastasia <nastia.iv.91@gmail.com>
2021-01-26 14:42:02 -0800

    sequelize added to serverjs file

--- server.js
+++ server.js
@@ -13,12 +13,12 @@ const mysql = require("mysql");
 let connection;
 // Connect to database
 if (process.env.JAWSDB_URL) {
-  var connection = mysql.createConnection(process.env.JAWSDB_URL)
+  var connection = new Sequelize(process.env.JAWSDB_URL)
 } else {
   var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'l@rG020979!',
+    password: '27qweasd',
     database: 'passport_demo'
   });
 }
@@ -35,6 +35,7 @@ if (process.env.JAWSDB_URL) {
 // Setting up port and requiring models for syncing
 const PORT = process.env.PORT || 8080;
 const db = require("./models");
+const { Sequelize } = require("./models");
 
 // Creating express app and configuring middleware needed for authentication
 const app = express();
@@ -54,6 +55,20 @@ app.use(passport.session());
 // Requiring our routes
 require("./routes/html-routes.js")(app);
 require("./routes/api-routes.js")(app);
+
+
+
+// Syncing our database and logging a message to the user upon success
+db.sequelize.sync().then(() => {
+  app.listen(PORT, () => {
+    console.log(
+      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+      PORT,
+      PORT
+    );
+  });
+});
+
 
 // // ========== NODEMAILER ========== //
 // const transporter = nodemailer.createTransport({
@@ -134,14 +149,3 @@ require("./routes/api-routes.js")(app);
 //     });
 // });
 // // ========== END NODEMAILER ========== //
-
-// Syncing our database and logging a message to the user upon success
-db.sequelize.sync().then(() => {
-  app.listen(PORT, () => {
-    console.log(
-      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-      PORT,
-      PORT
-    );
-  });
-});
